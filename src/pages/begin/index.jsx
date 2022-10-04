5335b8e79df560a21bfd9ac8d67de5d7b0346610
DEVRuivo <felipebuzzofranco5555@gmail.com>
2022-10-04 12:21:09 -0300

    aaa

--- src/pages/begin/index.jsx
+++ src/pages/begin/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import {Title} from "./style.js";
+import { Title, Header, Body } from "./style.js";
 
 export default function Begin() {
-    return (
-        <div>
-            <Title>Hello World!</Title>
-        </div>
-    );
+  return (
+    <div>
+      <Header>
+        <img src="" alt="Logo NFT" />
+        <a href="#">HOME</a>
+        <a href="#">ABOUT</a>
+        <a href="#">CONTACT</a>
+      </Header>
+      <div>
+        <Body>
+          <h1>12:03:24</h1>
+        </Body>
+      </div>
+    </div>
+  );
 }
-
-
-
--- src/pages/begin/style.js
+++ src/pages/begin/style.js
@@ -1,7 +1,28 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Title = styled.h1`
-font-size: 1.5em;
-text-align: center;
-color: palevioletred;
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+export const Header = styled.div`
+  width: auto;
+  height: 74px;
+  background: rgba(188, 18, 226, 0.22);
+  text-align: center;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  padding-left: 50px;
+  padding-right: 50px;
+`;
+
+export const Body = styled.div`
+  background-image: url("Background-begin.svg");
+  text-align: center;
+  width: auto;
+  height: 938px;
+  left: 0px;
+  top: 0px;
 `;
