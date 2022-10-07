c1f24a942ca78b0c1dceee4d36d05e268f8282bb
DEVRuivo <felipebuzzofranco5555@gmail.com>
2022-10-07 10:12:14 -0300

    Coment modfication

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Text, Title } from "./style.js";
 
 export default function About() {
-    return (
-        <div>
-            <h1>About</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Title>ABOUT</Title>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacinia
+        condimentum pharetra. Curabitur vitae egestas nisl, in placerat tortor.
+        Donec suscipit massa justo, ut pulvinar ipsum cursus volutpat. Nulla a
+        elementum mi. Duis quis lacus quis ligula malesuada venenatis ac sed
+        erat. Pellentesque blandit suscipit varius. Vivamus nec efficitur nisl.
+        Sed vel odio non orci sodales ornare. Phasellus molestie tincidunt
+        mauris. Proin sodales enim a nibh tincidunt, quis pellentesque lorem
+        vulputate.
+      </Text>
+    </div>
+  );
 }
---
+++ src/pages/about/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Text = styled.p`
+  width: auto;
+  height: 0;
+  padding-top: 3%;
+  text-align: center;
+
+`;
+
+export const Title = styled.h1`
+width: auto;
+text-align: center;
+
+`;
